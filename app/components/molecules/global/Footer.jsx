1e9045c6e59d5e2562507dbddeb14045091fc8d8
George Zikos <george.zikos@icloud.com>
2025-11-16 16:40:36 -0500

    Dialled in link interaction states

--- app/components/molecules/global/Footer.jsx
+++ app/components/molecules/global/Footer.jsx
@@ -12,11 +12,14 @@ export default function Footer({
             className={`text-text-secondary flex w-full justify-between ${className}`}
             {...rest} // Spread additional props to the footer element
         >
-            <p className="text-caption-sm-footer tabular-nums" data-element="footer__text">
+            <p
+                className="text-caption-sm-footer tabular-nums"
+                data-element="footer__text"
+            >
                 © {startingYear}–{currentYear}
             </p>
             <a
-                className="text-caption-sm-footer text-right no-underline"
+                className="text-caption-sm-footer hover:text-link-secondary-hover active:text-link-secondary-hover focus-visible:text-link-secondary-hover text-right no-underline transition-colors duration-200 ease-in-out"
                 data-element="footer__text"
                 href={`mailto:${contact}`}
                 aria-label={`Send email to ${contact}`}
--- app/components/organisms/global/Header.jsx
+++ app/components/organisms/global/Header.jsx
@@ -18,18 +18,20 @@ export default function Header({
                 className="pr-nav-gap-full sm:gap-x-grid-gutter flex w-full items-center justify-between sm:w-auto sm:pr-[0px]"
                 data-element="global-header__nav"
             >
-                <h1
-                    className="text-heading-lg-site-title text-text-primary sm:w-layout-left"
-                    data-element="global-header__name"
-                >
-                    <a
-                        href="/"
-                        className="no-underline"
-                        aria-label="George Zikos - Portfolio Home"
+                <div className="sm:w-layout-left">
+                    <h1
+                        className="text-heading-lg-site-title text-text-primary group w-fit"
+                        data-element="global-header__name"
                     >
-                        George Zikos
-                    </a>
-                </h1>
+                        <a
+                            href="/"
+                            className="group-hover:text-link-primary-hover group-active:text-link-primary-hover focus-visible:text-link-primary-hover no-underline transition-colors duration-200 ease-in-out"
+                            aria-label="George Zikos - Portfolio Home"
+                        >
+                            George Zikos
+                        </a>
+                    </h1>
+                </div>
                 {showNav && (
                     <MainNav
                         links={navigationLinks}
